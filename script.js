c4b00ffbbc34c9dca7c6aebc5fe530c8d21f2424
hetav <119531247+hetav98@users.noreply.github.com>
2024-08-02 17:30:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,19 @@ function showCopyNotification() {
         notification.style.display = "none";
     }, 3000);
 }
+//шо ты тут делаешь? не лезб! оно тебя сожрёт!!!
+document.addEventListener('DOMContentLoaded', function() {
+    const faqItems = document.querySelectorAll('.faq-question');
+
+    faqItems.forEach(item => {
+        item.addEventListener('click', function() {
+            const answer = this.nextElementSibling;
+            
+            if (answer.style.maxHeight) {
+                answer.style.maxHeight = null;
+            } else {
+                answer.style.maxHeight = answer.scrollHeight + 'px';
+            }
+        });
+    });
+});
